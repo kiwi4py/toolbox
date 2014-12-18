ba9fce898bc4f8025cd27d463ff98cb3fd440108
kiwipi <kiwidchx@live.cn>
2014-12-18 15:42:14 +0800

    add description
    add description

--- copyfile.py
+++ copyfile.py
@@ -1,4 +1,11 @@
 #coding: utf-8
+"""
+Editors have to enroll bookinfo into App Store which turns to be a tedious work. It's even worse because it's quite slow to 
+access the App Store platform. In order to help editors to save time as much as we can, we try to put each image file into a 
+foler named by its id. In this way, editors may easily find which image to upload. I wrote this tiny utility last year, but I
+just can't find it. I write it again and paste it here. Hopefully I will find it next time when I use it.
+"""
+
 import os
 import shutil
 
