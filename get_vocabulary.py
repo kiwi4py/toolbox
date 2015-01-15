02fd8c773db18c6fd811b28e6c7a215e493c02c9
kiwipi <kiwidchx@live.cn>
2015-01-15 10:47:52 +0800

    add annotations
    add annotations

--- get_vocabulary.py
+++ get_vocabulary.py
@@ -3,19 +3,23 @@ import nltk
 
 from os_file_util import get_name
 
-
-
 def get_a_book_content(bookname):
     book_content = open(bookname,"r+").read()
     return book_content
 
-def clean_book(bookcontent):
+def clean_book(book_content):
     pass
 
-def remove_name(bookcontent):
+def remove_name(book_content):
+    """
+    to remove name in nltk name corpus
+    """
     pass
 
-def remove_specifi_noun(bookcontent):
+def remove_specific_noun(book_content):
+    """
+    to remove specific noun, e.g. states, countries
+    """
     pass
     
 
@@ -27,10 +31,10 @@ def get_a_book_vocabulary(clean_book_content):
     return  book_vocabulary, vocabulary_num
 
 file_list = get_name(r"G:\pylab\export\export")
+#The following 3 lines should be useless if put py files in another directory
 file_list.remove("os_file_util.py")
 file_list.remove("os_file_util.pyc")
 file_list.remove("vocabulary.py")
-
 
 v = set()
 count = 1
