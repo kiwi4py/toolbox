4731e681ba721b45b33f394d10d25f0844349762
kiwipi <kiwidchx@live.cn>
2015-01-26 10:57:20 +0800

    Update statitics.py
    To explain why I wrote this.

--- statitics.py
+++ statitics.py
@@ -1,7 +1,16 @@
 #-*-coding:utf-8 -*-
+"""
+ When editor Gao gathers the data of a survey, she found it was quite boring to filter each column and
+ got the final result from a sheet like this:
+ question 1 - choice one | question 1 - choice tow | question 2 - choice one | question 2 - choice two ...
+ a real question looks like this "1.你的阅读出发点或动力主要是下面哪一项或哪些项？-个人兴趣"
+ She asked me for help.
+ After I made clear that she wanted data like this question 1 how many people choose choice one ...,
+ I wrote the following code to process the data:
+"""
 import csv
 
-title = "table head"
+title = "table head"     #  question 1 - choice one | question 1 - choice tow | question 2 - choice one | question 2 - choice two ...
 title = title.split()
 
 result = {}
@@ -13,7 +22,7 @@ for row in cr:
     for choice_num in range(0, len(row)):
         result[title[choice_num]].append((row[choice_num]))
 
-result  =  sorted(result.items(),key=lambda e: int(e[0].split('.')[0]),reverse=False)
+result  =  sorted(result.items(),key=lambda e: int(e[0].split('.')[0]),reverse=False) # sort the dict by number string in key
 
 for keys, values in result:
    print keys
