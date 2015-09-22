0a68f767cae1dcb5bfa7c5a184ba11e49b6a9630
kiwipi <kiwidchx@live.cn>
2015-09-22 16:01:41 +0800

    Update bookinfo.py

--- bookinfo.py
+++ bookinfo.py
@@ -5,15 +5,9 @@ from  openpyxl.cell  import  get_column_letter
 from  openpyxl.writer.excel  import  ExcelWriter
 from openpyxl.workbook import Workbook  
 
-bid = [3716,3715,3714,3711,2279, 2874, 2891, 2686, 3433, 32, 1618, 2941, 2888, 2279,
-       2224, 453, 2867, 2880, 2351, 2907, 2906, 2942, 3215, 3123, 2902, 2897, 2904, 50,
-       11, 14, 2893, 23, 2341, 2891, 1619, 2865, 2890, 2901, 1673, 504, 2896, 108, 216,
-       1, 455, 1238, 956, 943, 958, 1218, 533, 539, 542, 540, 538, 325, 607]
-
-
+bid = [3716,3715,3714,3711,2279]
 wb = load_workbook(filename='bookLIst.xlsx')
 sheet_ranges = wb['Sheet1']
-#
 #print sheet_ranges['A2'].value
 count_row = 1
 count_col = 1
