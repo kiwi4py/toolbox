b5d787ac08f66ab633314eb50bc0cba437c5f66f
kiwi4py <kiwiyisheng@sina.cn>
2015-10-10 10:22:25 +0800

    add lines to limit the upbound number of senses to 5

--- dictparse.py
+++ dictparse.py
@@ -19,19 +19,35 @@ def parseDict(tree):
                 category = body.findall("category")
                 for c in category:
                         senses = c.findall("sense")
+                        if len(senses) > 5:
+                                toremove = senses[5:]
+                                senses = senses[:5]
+                                for to in toremove:
+                                        c.remove(to)
                         for sense in senses:
                              examples = sense.findall("example")
                              for example in examples:
                                 sense.remove(example)
                 phrases = body.findall("phrase")
+                hyphs = body.findall("hyph")
+                grams = body.findall("gram")
+                usages = body.findall("usage")
+                derivatives = body.findall("derivative")
+                for h in hyphs:
+                        body.remove(h)
                 for p in phrases:
-                        senses = p.findall("sense")
-                        for sense in senses:
-                             examples = sense.findall("example")
-                             for example in examples:
-                                sense.remove(example)
+                    body.remove(p)
+
+                for g in grams:
+                        body.remove(g)
+
+                for u in usages:
+                        body.remove(u)
+
+                for d in derivatives:
+                        body.remove(d)
                         
-        tree.write('shawn-dict/' + filename, encoding="utf-8",xml_declaration=True) 
+        tree.write('shawn-dict-simplified/' + filename, encoding="utf-8",xml_declaration=True) 
 
 if __name__ == "__main__":
         filelist = ["12" + chr(number) + ".xml" for number in range(65, 91)]
