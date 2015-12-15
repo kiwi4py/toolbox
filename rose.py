43da06974059e1c66fff71d05a761de84381f56d
kiwi4py <kiwiyisheng@sina.cn>
2015-12-15 13:59:52 +0800

    count how many single items in the 2nd column and output the corresponding item in the 1st column

---
+++ rose.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+
+s = open("wangmei.txt", "r").read()
+s = s.split("\n")
+counted = []
+for i in s:
+    j = i.split("\t")
+    if j[-1] != "":
+        counted.append(j[-1])
+
+for item in set(counted):
+    left = ""
+    for j in s:
+        j = j.split("\t")
+        if j[-1] == item:
+            left = left + ";" + j[0]
+    print "%s \t %s \t %s" %(item, counted.count(item), left)
+
+        
+        
+       
+    
